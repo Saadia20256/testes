1b48adf36b4d162856572bf4b25de4b11a83e52a
Saadia20256 <haddachsaadia089@gmail.com>
2025-09-04 16:01:06 +0200

    chalnge 3

--- ex/day 2/chalnge 3.c
+++ ex/day 2/chalnge 3.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main () {
-    int n , s ;
-    printf("entrer un nombre entier positif :");
-    scanf ("%d",&n);
-    int somme = 1;
-    for(int i = 1 ; i<=n ;i++){
-      somme += i ;
-    }
+int main (){
+   int n ;
+   printf("entrer un valeur de n :");
+   scanf("%d" , &n);
 
-    printf("la somme des %d premier nombre naturel :%d \n", n , s);
-    return 0 ;
+   printf("%nombre premiers de 1 :\n", n);
+
+   for(int i = 2 ; i<=n ; i++ ){
+        int premier = 1 ; //en poose i est premiers
+   }
+   for(int j = 2 ; j*j<=i ; j++){
+      if(i%j==0){
+        premier = 0 ; //n'est pas un nombre premier
+        break ;
+      }
+   }
+   if(premier){
+    printf("%d",i) ;
+   }
+   printf("\n");
+
+   return 0 ;
 }
